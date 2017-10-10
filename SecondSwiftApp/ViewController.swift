dcb70df80ed11c48d3d73f910fd47330ec13e6da
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-10 08:01:50 -0600

    Made another color changing app

--- SecondSwiftApp/ViewController.swift
+++ SecondSwiftApp/ViewController.swift
@@ -10,16 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ColorButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBAction func colorChangeMethod(_ sender: UIButton)
+    {
+        ColorButton.backgroundColor = randomColor()
+        view.backgroundColor = randomColor()
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    private func randomColor() -> UIColor
+    {
+        let blueAmount = CGFloat (Double (arc4random_uniform(256)) / 255)
+        let redAmount = CGFloat (Double (arc4random_uniform(256)) / 255)
+        let greenAmount = CGFloat (Double (arc4random_uniform(256)) / 255)
+        return UIColor(red : redAmount, green : greenAmount, blue : blueAmount, alpha : CGFloat(1.0))
+    }
 
 }
 
