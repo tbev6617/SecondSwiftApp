c89e02261068b641a4af59bb1111efc365736fce
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-12 08:53:04 -0600

    Added another screen and Class

---
+++ SecondSwiftApp/Controller/SecondScreenController.swift
@@ -0,0 +1,14 @@
+//
+//  SecondScreenController.swift
+//  SecondSwiftApp
+//
+//  Created by Bevan, Tyler on 10/12/17.
+//  Copyright © 2017 Bevan, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class SecondScreenController : UIViewController
+{
+    
+}
