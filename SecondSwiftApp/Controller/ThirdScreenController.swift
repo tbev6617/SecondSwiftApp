9f7e31f8155d406a842ce6c8975a4bc7e3cb8344
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-10-16 09:17:53 -0600

    added a third view but haven't finished the segue

---
+++ SecondSwiftApp/Controller/ThirdScreenController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdScreenController.swift
+//  SecondSwiftApp
+//
+//  Created by Bevan, Tyler on 10/16/17.
+//  Copyright © 2017 Bevan, Tyler. All rights reserved.
+//
+
+import UIKit
+
+class ThirdScreenController: UIViewController
+{
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = colorTool.makeRandomColor()
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
